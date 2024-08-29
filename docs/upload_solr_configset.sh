b41adfceb84c1cd0562778355acad43f8fe57896
Kazi Abid Azad <kaziabidazad@gmail.com>
2024-08-29 17:16:37 +0530

    updated docs

--- docs/upload_solr_configset.sh
+++ docs/upload_solr_configset.sh
@@ -3,9 +3,10 @@
 # add an empty document and then start creating fields
 # Create collection using the configset just created
 # In the Admin UI, choose Add Copy Field, then fill out the source * and destination as _text_
-curl -X PUT -H "Content-Type:application/octet-stream" \
---data-binary @/mnt/d/works/workspaces/EclipseWorkspaces/POC/web-analytics-microservice/docs/wikipedia_configset.zip \
-"http://localhost:18983/api/cluster/configs/wikipedia"
+curl -X PUT -H "Content-Type:application/octet-stream" --data-binary @/mnt/d/works/workspaces/EclipseWorkspaces/POC/web-analytics-microservice/docs/wikipedia_configset.zip "http://localhost:18983/api/cluster/configs/wikipedia"
 
 # List all available configsets
 curl http://localhost:18983/api/cluster/configs/
+
+
+Query Solr to exclude a certain field: &fl=*,fullPage:[value%20v=""]
